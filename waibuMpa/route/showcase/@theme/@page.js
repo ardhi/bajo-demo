61feefa79490a847fba1b52e85caa0c34b72a823
Ardhi Lukianto <ardhi@lukianto.com>
2024-08-02 10:42:59 +0700

    Updates

--- waibuMpa/route/showcase/@theme/@page.js
+++ waibuMpa/route/showcase/@theme/@page.js
@@ -1,8 +1,8 @@
 async function showcase (ctx, req, reply) {
-  const { map } = this.app.bajo.lib._
+  const { map, pick } = this.app.bajo.lib._
   const { themes: allThemes } = this.app.waibuMpa
   req.theme = req.params.theme
-  const themes = map(allThemes, t => ({ name: t.name }))
+  const themes = map(allThemes, t => pick(t, ['name', 'framework']))
   const locals = { themes }
   return await reply.view(`bajoDemo:/showcase/${req.params.page}.html`, locals)
 }
