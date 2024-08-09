ca8b1925427576d285fa083f916a458ee2082137
Ardhi Lukianto <ardhi@lukianto.com>
2024-08-09 22:19:48 +0700

    Updates

--- waibuMpa/route/showcase/@theme/@iconset/@page.js
+++ waibuMpa/route/showcase/@theme/@iconset/@page.js
@@ -1,11 +1,11 @@
 async function showcase (ctx, req, reply) {
   const { map, pick } = this.app.bajo.lib._
-  const { themes: allThemes, iconsets: allIconsets } = this.app.waibuMpa
+  const { themes: allThemes, iconsets: allIconsets, mappingKeys } = this.app.waibuMpa
   req.theme = req.params.theme
   req.iconset = req.params.iconset
   const themes = map(allThemes, t => pick(t, ['name', 'framework']))
   const iconsets = map(allIconsets, t => pick(t, ['name']))
-  const locals = { themes, iconsets }
+  const locals = { themes, iconsets, mappingKeys }
   return await reply.view(`bajoDemo:/showcase/${req.params.page}.html`, locals)
 }
 
